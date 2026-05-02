8525169556a33ed967ddad32471ea68d9144dc50
dubalo <silee1122@gmail.com>
2026-05-02 10:43:33 +0900

    Add max_tokens and temperature to OpenAI initialization

--- main.py
+++ main.py
@@ -4,7 +4,10 @@
 from langchain_openai import OpenAI
 import streamlit as st 
 
-llm = OpenAI()
+llm = OpenAI(
+    max_tokens = 1000,
+    temperature = 0.7
+)
 st.title("내과 전문의")
 content = st.text_input("증상을 알려 주세요.")
 
@@ -12,4 +15,3 @@ if st.button("진료 요청하기"):
     with st.spinner("진료 중..."):
         result = llm.invoke(content + "증상에 대해 진료해주세요.")
         st.write(result)
-        max_tokens = 1000
